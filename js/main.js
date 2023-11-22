6c20191dc9726f3a1a5f53cae0d90757ebae40d0
Shouko <shouko4632@gmail.com>
2023-11-22 19:38:28 +0300

    Добавляет возможность редактировать изображение

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import {photosData} from './data.js';
-import {renderPictures} from './thumbnail-images.js';
+import { photosData } from './data.js';
+import { renderPictures } from './thumbnail-images.js';
 import './full-image.js';
 import './form-imgupload.js';
-
+import './image-editor.js';
 renderPictures(photosData);
 window.console.log(photosData);
 
